460ed4218f661c4219bd983dfcbb30a3564dcbaa
bouquet1 <bukuleta@gmail.com>
2023-06-03 11:54:40 -0700

    button toggles on/off when clicked

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false); //the initial state false, bc the button should be "OFF" when the component first renders.
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
